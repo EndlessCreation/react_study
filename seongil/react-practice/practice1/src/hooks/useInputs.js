d73e2589cd5e7b071c6cc10bb1ef805750508b88
Shin, Seong-il <callmeshin75@gmail.com>
2020-12-24 23:04:08 +0900

    message

---
+++ seongil/react-practice/practice1/src/hooks/useInputs.js
@@ -0,0 +1,14 @@
+import { useState, useCallback } from 'react';
+
+function useInputs(initialForm) {
+    const [form, setForm] = useState(initialForm);
+    //change
+    const onChange = useCallback(e=> {
+        const { name, value } = e.target;
+        setForm(form => ({ ...form, [name]: value }));
+    }, []);
+    const reset = useCallback(() => setForm(initialForm), [initialForm]);
+    return [form, onChange, reset];
+};
+
+export default useInputs;
